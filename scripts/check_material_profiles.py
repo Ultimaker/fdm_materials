508cffcd3c6464e867ad6495ca2ed8d141cc964d
Jaime van Kessel <nallath@gmail.com>
2018-11-23 13:37:31 +0100

    Refactoring the code to make it easier to read

--- scripts/check_material_profiles.py
+++ scripts/check_material_profiles.py
@@ -3,17 +3,14 @@ from collections import OrderedDict
 import os
 import sys
 import re
+from typing import Dict, Optional, List
 
 
 class MaterialProfilesValidator:
-
-    def __init__(self, root_dir: str):
-        self._repo_dir = os.path.abspath(root_dir)
-        self._materials_dir = self._repo_dir
-
+    def __init__(self) -> None:
         self._guid_pattern = re.compile(r"<GUID>.*</GUID>")
 
-    def _get_guid(self, content: str) -> str:
+    def _getGuid(self, content: str) -> str:
         guid = None
         for line in content.splitlines():
             line = line.strip()
@@ -22,44 +19,47 @@ class MaterialProfilesValidator:
                 break
         return guid
 
-    def get_materials_dir(self, dirpath: str):
+    def _getMaterialsDir(self, dirpath: str):
         for root_dir, dirnames, filenames in os.walk(dirpath):
             has_materials_file = any(fn.endswith(".xml.fdm_material") for fn in filenames)
             if not has_materials_file:
                 for dirname in dirnames:
                     full_dir_path = os.path.join(root_dir, dirname)
-                    return self.get_materials_dir(full_dir_path)
+                    return self._getMaterialsDir(full_dir_path)
 
             return dirpath
 
-    def validate(self) -> bool:
-        """
-        Validates the preset settings files and returns True or False indicating whether there are invalid files.
-        """
-        # parse the definition file
-        guid_dict = OrderedDict()
-
-        materials_dir = self.get_materials_dir(self._materials_dir)
-
-        # go through all the preset settings files
-        for _, _, filenames in os.walk(materials_dir):
+    #   Find all material files in a given directory.
+    #   This returns a dictionary with filename as keys and it's loaded content as value.
+    def _getAllMaterialsContentsInDir(self, directory: str) -> Dict[str, Optional[str]]:
+        result = {}
+        for _, _, filenames in os.walk(directory):
             for filename in filenames:
-                file_path = os.path.join(materials_dir, filename)
+                file_path = os.path.join(directory, filename)
                 if not filename.endswith(".xml.fdm_material"):
                     print("Skipping \"%s\"" % filename)
                     continue
-
-                with open(file_path, "r", encoding = "utf-8") as f:
-                    content = f.read()
-
-                guid = self._get_guid(content)
-                if guid not in guid_dict:
-                    guid_dict[guid] = []
-
-                item_list = guid_dict[guid]
-                item_list.append({"file_name": filename,
-                                  "file_path": file_path})
+                try:
+                    with open(file_path, "r", encoding = "utf-8") as f:
+                        result[filename] = f.read()
+                except:
+                    result[filename] = None
             break
+        return result
+
+    def validateAll(self, directory: str) -> bool:
+        materials_dir = self._getMaterialsDir(os.path.abspath(directory))
+
+        material_content_dict = self._getAllMaterialsContentsInDir(materials_dir)
+
+        # Store all the guid's linked with their filename. This is later used to find out if there are duplicate guids.
+        guid_dict = {}  # type: Dict[str, List[str]]
+
+        for file_name, material_content in material_content_dict.items():
+            guid = self._getGuid(material_content)
+            if guid not in guid_dict:
+                guid_dict[guid] = []
+            guid_dict[guid].append(file_name)
 
         has_invalid_files = False
         for guid, file_item_list in guid_dict.items():
@@ -72,7 +72,7 @@ class MaterialProfilesValidator:
             else:
                 print("-> The following files DO NOT contain any GUID:")
             for file_item in file_item_list:
-                print("    -- [%s]" % file_item["file_name"])
+                print("    -- [%s]" % file_item)
             print("-> PLEASE make sure to generate unique GUIDs for each material.")
 
         return not has_invalid_files
@@ -82,8 +82,8 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     root_dir = os.path.abspath(os.path.join(script_dir, ".."))
 
-    validator = MaterialProfilesValidator(root_dir)
-    is_everything_validated = validator.validate()
+    validator = MaterialProfilesValidator()
+    is_everything_validated = validator.validateAll(root_dir)
 
     ret_code = 0 if is_everything_validated else 1
     sys.exit(ret_code)
