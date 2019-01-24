375c99e578465f6073a400f6cbccf44c88c25bce
Lipu Fei <lipu.fei815@gmail.com>
2019-01-24 14:12:33 +0100

    Backup new script and restore the old one
    CURA-6128
    
    Adding lxml to the build environment takes way too much time. Decided to
    backup the new script and use the old one for now.

--- scripts/check_material_profiles.py
+++ scripts/check_material_profiles.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
+from collections import OrderedDict
 import os
 import sys
 import re
-from typing import Dict, Optional, List
-from lxml import etree
+
 
 class MaterialProfilesValidator:
-    def __init__(self) -> None:
+
+    def __init__(self, root_dir: str):
+        self._repo_dir = os.path.abspath(root_dir)
+        self._materials_dir = self._repo_dir
+
         self._guid_pattern = re.compile(r"<GUID>.*</GUID>")
 
-    def _getGuid(self, content: str) -> str:
+    def _get_guid(self, content: str) -> str:
         guid = None
         for line in content.splitlines():
             line = line.strip()
@@ -18,59 +22,46 @@ class MaterialProfilesValidator:
                 break
         return guid
 
-    def _getMaterialsDir(self, dirpath: str):
+    def get_materials_dir(self, dirpath: str):
         for root_dir, dirnames, filenames in os.walk(dirpath):
             has_materials_file = any(fn.endswith(".xml.fdm_material") for fn in filenames)
             if not has_materials_file:
                 for dirname in dirnames:
                     full_dir_path = os.path.join(root_dir, dirname)
-                    return self._getMaterialsDir(full_dir_path)
+                    return self.get_materials_dir(full_dir_path)
 
             return dirpath
 
-    #   Find all material files in a given directory.
-    #   This returns a dictionary with filename as keys and it's loaded content as value.
-    def _getAllMaterialsContentsInDir(self, directory: str) -> Dict[str, Optional[str]]:
-        result = {}
-        for _, _, filenames in os.walk(directory):
+    def validate(self) -> bool:
+        """
+        Validates the preset settings files and returns True or False indicating whether there are invalid files.
+        """
+        # parse the definition file
+        guid_dict = OrderedDict()
+
+        materials_dir = self.get_materials_dir(self._materials_dir)
+
+        # go through all the preset settings files
+        for _, _, filenames in os.walk(materials_dir):
             for filename in filenames:
-                file_path = os.path.join(directory, filename)
+                file_path = os.path.join(materials_dir, filename)
                 if not filename.endswith(".xml.fdm_material"):
                     print("Skipping \"%s\"" % filename)
                     continue
-                try:
-                    with open(file_path, "r", encoding = "utf-8") as f:
-                        result[filename] = f.read()
-                except:
-                    result[filename] = None
+
+                with open(file_path, "r", encoding = "utf-8") as f:
+                    content = f.read()
+
+                guid = self._get_guid(content)
+                if guid not in guid_dict:
+                    guid_dict[guid] = []
+
+                item_list = guid_dict[guid]
+                item_list.append({"file_name": filename,
+                                  "file_path": file_path})
             break
-        return result
 
-    def validateAll(self, directory: str) -> bool:
-        materials_dir = self._getMaterialsDir(os.path.abspath(directory))
-
-        material_content_dict = self._getAllMaterialsContentsInDir(materials_dir)
-    
-        # Store all the guid's linked with their filename. This is later used to find out if there are duplicate guids.
-        guid_dict = {}  # type: Dict[str, List[str]]
-        xmlschema_doc = etree.parse("fdmmaterial.xsd")
-        xmlschema = etree.XMLSchema(xmlschema_doc)
         has_invalid_files = False
-
-        for file_name, material_content in material_content_dict.items():
-            guid = self._getGuid(material_content)
-            if guid not in guid_dict:
-                guid_dict[guid] = []
-            guid_dict[guid].append(file_name)
-
-            xml_doc = etree.fromstring(material_content.encode())
-            try:
-                xmlschema.assertValid(xml_doc)
-            except etree.DocumentInvalid as e:
-                has_invalid_files = True
-                print("{file_name} is not a valid fdm material".format(file_name = file_name))
-                print(e)
-
         for guid, file_item_list in guid_dict.items():
             if len(file_item_list) <= 1:
                 continue
@@ -81,7 +72,7 @@ class MaterialProfilesValidator:
             else:
                 print("-> The following files DO NOT contain any GUID:")
             for file_item in file_item_list:
-                print("    -- [%s]" % file_item)
+                print("    -- [%s]" % file_item["file_name"])
             print("-> PLEASE make sure to generate unique GUIDs for each material.")
 
         return not has_invalid_files
@@ -91,8 +82,8 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     root_dir = os.path.abspath(os.path.join(script_dir, ".."))
 
-    validator = MaterialProfilesValidator()
-    is_everything_validated = validator.validateAll(root_dir)
+    validator = MaterialProfilesValidator(root_dir)
+    is_everything_validate = validator.validate()
 
-    ret_code = 0 if is_everything_validated else 1
+    ret_code = 0 if is_everything_validate else 1
     sys.exit(ret_code)
---
+++ scripts/check_material_profiles_new_with_lxml.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+from typing import Dict, Optional, List
+from lxml import etree
+
+class MaterialProfilesValidator:
+    def __init__(self) -> None:
+        self._guid_pattern = re.compile(r"<GUID>.*</GUID>")
+
+    def _getGuid(self, content: str) -> str:
+        guid = None
+        for line in content.splitlines():
+            line = line.strip()
+            if self._guid_pattern.match(line):
+                guid = line.strip("<GUID>").strip("</GUID>")
+                break
+        return guid
+
+    def _getMaterialsDir(self, dirpath: str):
+        for root_dir, dirnames, filenames in os.walk(dirpath):
+            has_materials_file = any(fn.endswith(".xml.fdm_material") for fn in filenames)
+            if not has_materials_file:
+                for dirname in dirnames:
+                    full_dir_path = os.path.join(root_dir, dirname)
+                    return self._getMaterialsDir(full_dir_path)
+
+            return dirpath
+
+    #   Find all material files in a given directory.
+    #   This returns a dictionary with filename as keys and it's loaded content as value.
+    def _getAllMaterialsContentsInDir(self, directory: str) -> Dict[str, Optional[str]]:
+        result = {}
+        for _, _, filenames in os.walk(directory):
+            for filename in filenames:
+                file_path = os.path.join(directory, filename)
+                if not filename.endswith(".xml.fdm_material"):
+                    print("Skipping \"%s\"" % filename)
+                    continue
+                try:
+                    with open(file_path, "r", encoding = "utf-8") as f:
+                        result[filename] = f.read()
+                except:
+                    result[filename] = None
+            break
+        return result
+
+    def validateAll(self, directory: str) -> bool:
+        materials_dir = self._getMaterialsDir(os.path.abspath(directory))
+
+        material_content_dict = self._getAllMaterialsContentsInDir(materials_dir)
+    
+        # Store all the guid's linked with their filename. This is later used to find out if there are duplicate guids.
+        guid_dict = {}  # type: Dict[str, List[str]]
+        xmlschema_doc = etree.parse("fdmmaterial.xsd")
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+        has_invalid_files = False
+
+        for file_name, material_content in material_content_dict.items():
+            guid = self._getGuid(material_content)
+            if guid not in guid_dict:
+                guid_dict[guid] = []
+            guid_dict[guid].append(file_name)
+
+            xml_doc = etree.fromstring(material_content.encode())
+            try:
+                xmlschema.assertValid(xml_doc)
+            except etree.DocumentInvalid as e:
+                has_invalid_files = True
+                print("{file_name} is not a valid fdm material".format(file_name = file_name))
+                print(e)
+
+        for guid, file_item_list in guid_dict.items():
+            if len(file_item_list) <= 1:
+                continue
+            has_invalid_files = True
+
+            if guid is not None:
+                print("-> The following files contain the same GUID [%s]:" % guid)
+            else:
+                print("-> The following files DO NOT contain any GUID:")
+            for file_item in file_item_list:
+                print("    -- [%s]" % file_item)
+            print("-> PLEASE make sure to generate unique GUIDs for each material.")
+
+        return not has_invalid_files
+
+
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    root_dir = os.path.abspath(os.path.join(script_dir, ".."))
+
+    validator = MaterialProfilesValidator()
+    is_everything_validated = validator.validateAll(root_dir)
+
+    ret_code = 0 if is_everything_validated else 1
+    sys.exit(ret_code)
