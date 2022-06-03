e04f9b522fe3d0842abc495729f6b5c4a288e726
Onno Broekmans <o.broekmans@ultimaker.com>
2022-06-03 11:08:34 +0200

    Improve check_material_profiles script
    - Use `argparse` to let the script take command line arguments. This
    allows for customizing the files that are to be checked (useful for
    checking files in other repos, such as `fdm_materials_certified`).
    
    - Improve output. The `-v` flag now increases output verbosity, and
    shows names of files that are being checked.
    
    - Separate validation logic from file processing logic and file loading
    logic.

--- scripts/check_material_profiles.py
+++ scripts/check_material_profiles.py
@@ -1,164 +1,157 @@
 #!/usr/bin/env python3
-# This script is dedicated to the public domain under the terms of the CC0 license.
+"""
+Validation for `*.xml.fdm_material` Material Profile files, based on an XML Schema
+Definition.
 
+This script is dedicated to the public domain under the terms of the CC0 license.
+"""
+
+import logging
 import os
+from pathlib import Path
 import sys
-import re
-from typing import Dict, Optional, List
+from typing import Dict, Iterable, Optional, List
 
 from lxml import etree
 
 
-##  This is a material profile validator that works with the lxml library.
+NAMESPACES = {
+    "um": "http://www.ultimaker.com/material",
+    "cura": "http://www.ultimaker.com/cura",
+}
+
+
+class MaterialProfile:
+
+    def __init__(self, document: etree.Element, filename: Optional[Path]) -> None:
+        self.document = document
+        self.filename = filename
+
+    @classmethod
+    def from_file(cls, p: Path) -> 'MaterialProfile':
+        with p.open("rb") as f:
+            return cls(etree.fromstring(f.read()), p)
+
+    def brand(self) -> Optional[str]:
+        """Get the material brand name from the given lxml.etree root node.
+
+        @returns None if the brand cannot be found, otherwise the brand text.
+        """
+        node = self.document.xpath("./um:metadata/um:name/um:brand", namespaces=NAMESPACES)
+        return node[0].text if node else None
+
+    def guid(self) -> Optional[str]:
+        node = self.document.xpath("./um:metadata/um:GUID", namespaces=NAMESPACES)
+        return node[0].text if node else None
+
+
 class MaterialProfilesValidator:
+    """Material Profile validator that validates against an XML Schema"""
 
-    __namespaces = {
-        "um": "http://www.ultimaker.com/material",
-        "cura": "http://www.ultimaker.com/cura",
-    }
+    class ValidationError(Exception):
+        pass
 
-    def __init__(self) -> None:
-        self._guid_pattern = re.compile(r"<GUID>.*</GUID>")
+    def __init__(self, xsd_file: Path) -> None:
+        self._schema = self.load_schema(xsd_file)
+        self._guids_seen: Dict[str, Path] = dict()
 
-    def _getGuid(self, content: str) -> str:
-        guid = None
-        for line in content.splitlines():
-            line = line.strip()
-            if self._guid_pattern.match(line):
-                guid = line.strip("<GUID>").strip("</GUID>")
-                break
-        return guid
+    @staticmethod
+    def load_schema(xsd_file: Path) -> etree.XMLSchema:
+        xmlschema_doc = etree.parse(xsd_file)
+        return etree.XMLSchema(xmlschema_doc)
 
-    def _getMaterialsDir(self, dirpath: str) -> str:
-        for root_dir, dirnames, filenames in os.walk(dirpath):
-            has_materials_file = any(fn.endswith(".xml.fdm_material") for fn in filenames)
-            if not has_materials_file:
-                for dirname in dirnames:
-                    full_dir_path = os.path.join(root_dir, dirname)
-                    return self._getMaterialsDir(full_dir_path)
+    def validate(self, profile: MaterialProfile) -> None:
+        """Validate the given material profile file against the XML schema, plus additional rules
 
-            return dirpath
+        Additional rules:
+        - Only material profile files whose name starts with "generic_" are allowed to have
+          their brand set to "Generic".
+        - The GUID in each profile should be unique; no other profiles can use the same GUID.
 
-    #   Find all material files in a given directory.
-    #   This returns a dictionary with filename as keys and it's loaded content as value.
-    def _getAllMaterialsContentsInDir(self, directory: str) -> Dict[str, Dict[str, str]]:
-        result = {}  # type: Dict[str, Dict[str, str]]
-        for _, _, filenames in os.walk(directory):
-            for filename in filenames:
-                file_path = os.path.join(directory, filename)
-                if not filename.endswith(".xml.fdm_material"):
-                    continue
+        @raises ValidationError if any problems are found.
+        """
+        # Validate the file content with the XSD file.
+        try:
+            self._schema.assertValid(profile.document)
+        except etree.DocumentInvalid as e:
+            raise self.ValidationError(f"{profile.filename} is not a valid FDM Material file:\n{e}")
 
-                result[filename] = {"content": "",
-                                    "error": ""}
-                try:
-                    with open(file_path, "r", encoding = "utf-8") as f:
-                        result[filename]["content"] = f.read()
-                except Exception as e:
-                    print("Failed to read file [%s] : %s", filename, e)
-                    result[filename]["error"] = str(e)
-            break
-        return result
+        # Make sure that only the material files such as "generic_<bla>" can have brand "Generic".
+        brand = profile.brand()
+        if brand is None:
+            raise self.ValidationError(f"{profile.filename} is missing '<brand>' information")
+        elif len(brand) == 0:
+            raise self.ValidationError(f"{profile.filename} contains empty '<brand>' information")
+        elif brand.lower() == "generic" and not profile.filename.name.lower().startswith("generic_"):
+            raise self.ValidationError(
+                f"{profile.filename} contains a 'generic' brand, but only material profiles with a "
+                f"filename that starts with 'generic_' are allowed to have a generic brand set.")
 
-    # Gets the material brand name from the given lxml.etree root node. Return None if the brand cannot be found,
-    # otherwise the brand text.
-    def _getMaterialBrandName(self, root_node: "etree.Element") -> Optional[str]:
-        node = root_node.xpath("./um:metadata/um:name/um:brand", namespaces = self.__namespaces)
-        if not node:
-            return None
+        # Check that we haven't seen this GUID before.
+        guid = profile.guid()
+        if not guid:
+            raise self.ValidationError(f"{profile.filename} is missing '<GUID>' information")
+        if guid in self._guids_seen and self._guids_seen.get(guid) != profile.filename:
+            raise self.ValidationError(
+                f"{profile.filename} has duplicate GUID '{guid}', the same GUID is also "
+                f"used by: {self._guids_seen[guid]}")
+        self._guids_seen[guid] = profile.filename
 
-        return node[0].text
 
-    def validateAll(self, repo_root_dir: str) -> bool:
-        repo_root_dir = os.path.abspath(repo_root_dir)
+def validate_files(xsd_file: Path, files_to_check: Iterable[Path]) -> bool:
+    """Validate a given list of FDM material profile files using the given XSD schema file
 
-        materials_dir = self._getMaterialsDir(repo_root_dir)
-        fdmmaterial_xsd_file_path = os.path.join(repo_root_dir, "scripts", "fdmmaterial.xsd")
+    @returns Whether all files were validated successfully
+    """
+    validator = MaterialProfilesValidator(xsd_file)
 
-        material_content_dict = self._getAllMaterialsContentsInDir(materials_dir)
+    n_success, n_fail = 0, 0
 
-        # Store all the guid's linked with their filename. This is later used to find out if there are duplicate guids.
-        guid_dict = {}  # type: Dict[str, List[str]]
-        xmlschema_doc = etree.parse(fdmmaterial_xsd_file_path)
-        xmlschema = etree.XMLSchema(xmlschema_doc)
-        has_invalid_files = False
+    for f in files_to_check:
+        try:
+            logging.info(f"Checking {f.name}")
+            profile = MaterialProfile.from_file(f)
+            validator.validate(profile)
+        except etree.XMLSyntaxError as e:
+            logging.error(f"{f} is not a valid XML file:\n{e}")
+            n_fail += 1
+        except MaterialProfilesValidator.ValidationError as e:
+            logging.error(str(e))
+            n_fail += 1
+        else:
+            n_success += 1
 
-        for file_name, file_info_dict in material_content_dict.items():
-            # Show error message if the file failed to load
-            if len(file_info_dict["error"]) > 0:
-                print("{file_name} failed to load, error: {error}".format(file_name = file_name,
-                                                                          error = file_info_dict["error"]))
-                has_invalid_files = True
-                continue
-
-            material_content = file_info_dict["content"]
-
-            try:
-                xml_doc = etree.fromstring(material_content.encode())
-            except etree.XMLSyntaxError as e:
-                print("{file_name} contains XML syntax error".format(file_name = file_name))
-                print(e)
-                has_invalid_files = True
-                continue
-
-            # Validate the file content with the XSD file.
-            guid = self._getGuid(material_content)
-            if guid not in guid_dict:
-                guid_dict[guid] = []
-            guid_dict[guid].append(file_name)
-            try:
-                xmlschema.assertValid(xml_doc)
-            except etree.DocumentInvalid as e:
-                has_invalid_files = True
-                print("{file_name} is not a valid fdm material".format(file_name = file_name))
-                print(e)
-                continue
-
-            # Make sure that only the material files such as "generic_<bla>" can have branch "Generic".
-            brand = self._getMaterialBrandName(xml_doc)
-            if brand is None:
-                has_invalid_files = True
-                print("{file_name} Could not find <brand>".format(file_name = file_name))
-                continue
-            if len(brand) == 0:
-                has_invalid_files = True
-                print("{file_name} contains an empty string for <brand>".format(file_name = file_name))
-                continue
-            if brand.lower() == "generic" and not file_name.lower().startswith("generic_"):
-                has_invalid_files = True
-                print("{file_name} has brand [{brand}] but only material files named as [generic*] can have brand [Generic]".format(
-                      file_name = file_name, brand = brand))
-                continue
-
-        # Check for duplicate GUIDs
-        for guid, file_item_list in guid_dict.items():
-            if len(file_item_list) <= 1:
-                continue
-            has_invalid_files = True
-
-            if guid is not None:
-                print("-> The following files contain the same GUID [%s]:" % guid)
-            else:
-                print("-> The following files DO NOT contain any GUID:")
-            for file_item in file_item_list:
-                print("    -- [%s]" % file_item)
-            print("-> PLEASE make sure to generate unique GUIDs for each material.")
-
-        return not has_invalid_files
+    logging.info(f"Checked {n_success + n_fail} file(s): {n_success} OK, {n_fail} error(s)")
+    return (n_fail == 0)
 
 
 def main():
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    root_dir = os.path.abspath(os.path.join(script_dir, ".."))
+    import argparse
 
-    validator = MaterialProfilesValidator()
-    is_everything_validated = validator.validateAll(root_dir)
+    SCRIPT_DIR = Path(__file__).parent.resolve()
+    PROJECT_DIR = SCRIPT_DIR.parent
 
-    if is_everything_validated:
-        print("All material profiles seem valid.")
+    parser = argparse.ArgumentParser(
+        description="Validator for Material Profile (*.xml.fdm_material) files")
+    parser.add_argument("-x", "--xsd", default=(SCRIPT_DIR / "fdmmaterial.xsd"),
+                        help="XML Schema Definition file to use for validation (default: fdmmaterial.xsd)")
+    parser.add_argument("file", metavar="FILE", nargs="*",
+                        help="One or more *.xml.fdm_material files to check. Default: all files in project root.")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Increase output verbosity.")
+    args = parser.parse_args()
 
-    ret_code = 0 if is_everything_validated else 1
-    sys.exit(ret_code)
+    logging.basicConfig(
+        format="%(levelname)s %(message)s",
+        level=(logging.INFO if args.verbose else logging.WARNING))
+
+    if args.file:
+        files_to_check = [Path(_) for _ in args.file]
+    else:
+        files_to_check = sorted(list(PROJECT_DIR.glob("*.fdm_material")))
+
+    success = validate_files(args.xsd, files_to_check)
+    sys.exit(0 if success else 1)
 
 
 if __name__ == "__main__":
